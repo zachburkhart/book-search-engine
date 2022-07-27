8d2954980147cfe3cbdb746875eb7d2ac33377b7
Zach Burkhart <zachburkhart216@gmail.com>
2022-07-26 21:10:10 -0400

    built out typeDefs.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    bookId: String
+    authors: String
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input SavedBook_ {
+    bookId: String
+    authors: String
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    removeBook(bookId: String!): User
+    saveBook(book: SavedBook_): User
+  }
+`;
+
+module.exports = typeDefs;
