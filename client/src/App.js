9af803a749424c5bb3e7b4232423f603ec805e41
Zach Burkhart <zachburkhart216@gmail.com>
2022-07-26 23:02:04 -0400

    fixed app.js with http and auth link

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,20 @@ import {
   ApolloProvider,
 } from '@apollo/client';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
